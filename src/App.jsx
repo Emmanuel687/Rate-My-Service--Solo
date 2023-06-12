77cb5842dce3d29e6abb7acb39d49c36eb9dbfb7
Emmanuel Koech <93251478+Emmanuel687@users.noreply.github.com>
2023-06-12 22:10:14 +0300

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import LandingPage from './components/landingPage/LandingPage';
+import Header from './components/navigation/Header';
+
+function App() {
+
+  return (
+    <>
+     <Header />
+     <LandingPage />
+
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/landingPage/LandingPage.jsx
@@ -0,0 +1,20 @@
+
+export default function index() {
+    return (
+        <div className>
+            <div className="dark:bg-gray-900">
+                <section className="mx-auto container w-full py-36">
+                    <div className="flex flex-col justify-center items-center">
+                        <div className="md:text-5xl text-4xl font-black text-center text-gray-800 dark:text-white leading-snug lg:w-3/4">
+                            <h2> We help brands to measure and monitor experience analytics</h2>
+                        </div>
+                        <div className="flex justify-center items-center mt-16">
+                            <button className="focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-800 hover:opacity-90 w-48 h-12 text-lg text-white bg-gradient-to-l from-indigo-600 to-indigo-700 rounded">Sign Up </button>
+                        </div>
+                    </div>
+                </section>
+            </div>
+        </div>
+
+    );
+}
